88fb00fbf0f4db5a1fd3767fedd308841de5edb3
LEE SEOJUN <junbeom95@gmail.com>
2025-12-30 11:48:43 +0900

    yml 수정

--- src/main/kotlin/nbc/docker/sampleapp/RedisConfig.kt
+++ src/main/kotlin/nbc/docker/sampleapp/RedisConfig.kt
@@ -1,18 +1,14 @@
-package nbc.docker.sampleapp;
+package nbc.docker.sampleapp
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.StringRedisTemplate
 
-@Slf4j
 @Configuration
-public class RedisConfig {
-
+class RedisConfig {
     @Bean
-    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        log.info("RedisConfig : : redisConnectionFactory : ping : {} ", redisConnectionFactory.getConnection().ping());
-        return new StringRedisTemplate(redisConnectionFactory);
+    fun stringRedisTemplate(redisConnectionFactory: RedisConnectionFactory): StringRedisTemplate {
+        return StringRedisTemplate(redisConnectionFactory)
     }
 }
